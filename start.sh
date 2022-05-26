4d212981caf0df54386510714f8fc1441aa3bb64
thatretrodev <thatretrodev@outlook.com>
2022-05-26 19:02:32 -0400

    Switch to tuxmachine-manager

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d manager/desktop-app/dist ]
+then
+	cd manager;./build.sh;cd ..
+fi
+
 Xephyr -screen 1280x720 :8999 -terminate &
 sleep 0.1
-DISPLAY=:8999 xterm -fullscreen -e python3 menu.py
+DISPLAY=:8999 ./manager/desktop-app/dist/tuxmachine-manager-linux-x64/tuxmachine-manager
